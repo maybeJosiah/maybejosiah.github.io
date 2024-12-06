76f22151f6eb3987b91e44423fcfa861b69f1ae5
maybeJosiah <maybejosiah@aol.com>
2024-12-06 16:03:27 -0500

    Finalize maybe
    Add all things for end maybe or maybe not, maybe.

---
+++ scripts/contact.js
@@ -0,0 +1,6 @@
+//Make form element display a message upon submit.
+document.getElementById("form").addEventListener("submit",function() {
+alert("This form does not actually contact me, can you use methods above?");
+//X E
+});
+//X E
